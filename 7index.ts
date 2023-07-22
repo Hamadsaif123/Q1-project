371e38ad4ceba4a8bbceabafde147bffdedd63aa
Hamadsaif123 <140234004+Hamadsaif123@users.noreply.github.com>
2023-07-22 18:17:23 +0500

    Add files via upload

---
+++ 7index.ts
@@ -0,0 +1,15 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//adding
+const add = 4+4
+console.log("Additon result",add)
+//substracting
+const substracting = 16-8
+console.log("substracting",substracting)
+//multiplying
+const multiplication = 2*4
+console.log("multipliction",multiplication)
+//division
+const divide = 64/8;
+console.log("dividing",divide)
+
